8a4cd3f0438c5766eaa4e6b6fd4f72e9cb594658
johanvasquezdev <20251235@itla.edu.do>
2026-07-12 11:55:35 -0400

    Agregar DataAnnotations a los ViewModels

--- RealEstateApp.Core.Application/ViewModels/Properties/FilterPropertyViewModel.cs
+++ RealEstateApp.Core.Application/ViewModels/Properties/FilterPropertyViewModel.cs
@@ -1,10 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RealEstateApp.Core.Application.ViewModels.Properties;
 
 public class FilterPropertyViewModel
 {
+    [Display(Name = "Tipo de Propiedad")]
     public int? PropertyTypeId { get; set; }
+
+    [Display(Name = "Precio Mínimo")]
+    [DataType(DataType.Currency)]
     public decimal? MinPrice { get; set; }
+
+    [Display(Name = "Precio Máximo")]
+    [DataType(DataType.Currency)]
     public decimal? MaxPrice { get; set; }
+
+    [Display(Name = "Cantidad de Habitaciones")]
     public int? Rooms { get; set; }
+
+    [Display(Name = "Cantidad de Baños")]
     public int? Bathrooms { get; set; }
 }
