d4cf06bb8fdab8193a68d774a787f2f0d7e827ff
johanvasquezdev <20251235@itla.edu.do>
2026-07-20 11:59:34 -0400

    Show profile photo in agent chat detail and chat index views

--- RealEstateApp.Core.Application/Services/ChatService.cs
+++ RealEstateApp.Core.Application/Services/ChatService.cs
@@ -66,6 +66,7 @@ public class ChatService : IChatService
         {
             ClientId = clientId,
             ClientName = client is not null ? $"{client.FirstName} {client.LastName}" : "Desconocido",
+            ClientPhoto = client?.ProfilePictureUrl,
             PropertyId = propertyId,
             PropertyCode = property?.Code ?? "",
             Messages = messages.Select(m => new MessageViewModel
